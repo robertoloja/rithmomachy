4ae3d21d5ce133e1818296339976b5f77fae41b8
Roberto Loja <robertoloja@gmail.com>
2016-02-11 04:05:11 -0500

    Improved documentation.

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 /**
  * The game board is a 2D array, containing either Piece objects or 0.
+ *
+ * N.B.: JavaScript, being JavaScript, has no reliable way to compare 
+ *		 inner arrays in 2D arrays. The hack used throughout is to iterate
+ *		 over the outer array, then Array.toString() the inner arrays to
+ *		 compare with other Array.toString()'ed functions.
+ * 
+ * This file is included in the HTML document, at the top. Thus, it defines
+ * gloval variables for the project.
  **/
 var gameBoard = [
 	[0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,9 +30,10 @@ var gameBoard = [
 
 var col_labels = " abcdefgh".split("");
 
+
 /**
- * This file is included in the HTML document, at the top. Thus, it defines
- * gloval variables for the project.
+ * Generates the HTML board, naming each row after its number (labels are _x,
+ * where x is the number) and each cell as, e.g., "a16" for gameBoard[0][0].
  **/
 function generateBoard() {
 	document.write("<table class='game-board'>");
@@ -52,17 +61,25 @@ function generateBoard() {
 	document.write("</table>");
 }
 
-function setupBoard() {
+/**
+ * Iterates over the entire gameBoard, clearing cells filled with 0 and
+ * drawing cells that have game pieces in them.
+ *
+ * TODO: Currently only handles Rounds.
+ **/
+function drawPieces() {
 	for (var row = 0; row < gameBoard.length; row++) {
 		for (var col = 0; col < gameBoard[0].length; col++) {
+			var cell = document.getElementById(col_labels[col + 1] +
+								(16 - row).toString());
 			if (gameBoard[row][col] !== 0) {
 				console.log((16 - row).toString());
 				console.log(col_labels[col + 1]);
-				var cell = document.getElementById(
-						   col_labels[col + 1] + (16 - row).toString());
 				cell.innerHTML = "<div id='" + cell.id + "piece' " +
 								 "class='round'>" + gameBoard[row][col].value+
 								 "</div>";
+			} else {
+				cell.innerHTML = "";
 			}
 		}
 	}
