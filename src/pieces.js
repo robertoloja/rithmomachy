55810cae6c74637a242c80ef63d983bbc0a9f59a
Roberto Loja <robertoloja@gmail.com>
2016-02-13 20:46:21 -0500

    Added the skeleton of the Pyramid objects. Still much to do.

--- src/pieces.js
+++ src/pieces.js
@@ -308,8 +308,8 @@ Square.prototype.move = function(destination) {
 
 
 /**
- * Populates Piece.possibleMoves with moves that are on the board, follow this
- * piece's movement rules, and land on empty squares.
+ * Populates Piece.possibleMoves with moves that are on the board and follow
+ * this piece's movement rules.
  */
 Square.prototype.findLegalMoves = function () {
 	var candidateMoves = {
@@ -342,4 +342,46 @@ Square.prototype.findLegalMoves = function () {
 	
 	this.possibleMoves.normal = candidateMoves.normal;
 	this.possibleMoves.flying = candidateMoves.flying;
+};
+
+/*************************************************************************/
+
+/**
+ * Represents a pyramidal game piece. 
+ * @classdesc Pyramids contain pieces of other types, and can move in the same
+ *			  way as any of its constituent pieces. The pyramid's starting
+ *			  value is the sum of their constituents.
+ *			  Light values: 15, 25, 45, 81,  169, 153, 289.
+ *			  Dark values:  28, 49, 66, 120, 121, 225, 361.
+ * @constructor
+ * @extends Piece
+ * @param {string} color - Light or dark; which player controls this piece.
+ * @param {number[]} position - Position on the board. Two integers, [x, y].
+ * @param {Piece[]} constituents - All the Pieces that make up this pyramid.
+ */
+
+function Pyramid(color, position, constituents) {
+	Piece.call(this, color, position);
+	this.value = value;
+	this.constituents = constituents; 
+	this.addToBoard(position);
+	this.findLegalMoves();
+}
+
+Pyramid.prototype = Object.create(Piece.prototype);
+Pyramid.prototype.constructor = Pyramid;
+
+Pyramid.prototype.add = function (piece) {
+	this.constituents.push(piece);
+	this.value += piece.value;
+};
+
+Pyramid.prototype.remove = function (piece) {
+	// I'll have to find the piece, by shape and value.
+};
+
+Pyramid.prototype.findLegalMoves = function () {
+	for (var piece of this.constituents) {
+		this.possibleMoves = piece.possibleMoves;
+	}
 };
