b919b930470ce23c1ef0ca78e6421c0ff2beb4f6
Roberto Loja <robertoloja@gmail.com>
2016-03-14 09:12:09 -0400

    Now using Express for the server.

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-http = require('http');
-fs = require('fs');
+const express = require('express');
+const app = express();
 
-function send404Response(response) {
-	response.writeHead(404, {"Content-Type": "text/plain"});
-	response.write("Woops, don't got.");
-	response.end();
-}
+app.get('/', (req, res) => {
+  console.log('Server is running.');
+  res.send('working');
+});
 
-function onRequest(request, response) {
-	if (request.method == 'GET' && request.url == '/') {
-		response.writeHead(200, {"Content-Type": "text/html"});
-		fs.createReadStream("./index.html").pipe(response);
-	} else {
-		send404Response(response);
-	}
-}
-
-http.createServer(onRequest).listen(8000);
-console.log("Server running.");
+app.listen(3000);
