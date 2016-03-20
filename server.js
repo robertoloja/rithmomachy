f65f67e88fce2a93b8ae74da69ce33cfcdaa41f8
Roberto Loja <robertoloja@gmail.com>
2016-03-20 10:58:45 -0400

    Started writing the Express server.

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-  console.log('Server is running.');
-  res.send('working');
+app.use('/angular', express.static(__dirname + '/node_modules/angular/'));
+app.use('/css', express.static(__dirname + '/style/'));
+app.use('/public', express.static(__dirname + '/public/'));
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log('Server is running on port %s.', PORT);
+});
