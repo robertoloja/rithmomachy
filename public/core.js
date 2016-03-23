170eb6c0d0052e623e2f3531068e72041ae33c24
Roberto Loja <robertoloja@gmail.com>
2016-03-22 23:29:21 -0400

    Minor.

--- public/core.js
+++ public/core.js
@@ -1,7 +1,6 @@
 /**
  * N.B.: This version of Angular does not support arrow functions. 
- *       If you wrap everything in a closure, DON'T FORGET the () at the
- *       end, because that auto-executes the function. 
+ *       If you wrap everything in a closure, DON'T FORGET the () at the end.
  */
 (function() {
   const app = angular.module('mainScreen', []);
