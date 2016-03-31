8180db0ff702415a71f0693bd7329dd8cb29a1d3
Roberto Loja <robertoloja@gmail.com>
2016-03-31 00:10:05 -0400

    Minor interface work.

--- public/core.js
+++ public/core.js
@@ -39,6 +39,7 @@
   /**
    * Piece directives.
    */
+  // TODO: add the listeners to this to make it move
   app.directive('round', () => {
     return {
       restrict: 'E',
@@ -55,18 +56,33 @@
           width: '32px',
           height: '32px',
           borderRadius: '16px',
-          textAlign: 'center',
           boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.8)',
         });
       },
     };
   });
 
+  // TODO: solve the drop-shadow and text issue
   app.directive('triangle', () => {
     return {
       restrict: 'E',
       transclude: true,
+      scope: {
+        color: '=color',
+      },
       template: '<div ng-transclude></div>',
+      link: (scope, element) => {
+        element.css({
+          position: 'relative',
+          display: 'block',
+          width: '0',
+          height: '0',
+          borderStyle: 'solid',
+          borderWidth: '0 17px 29.4px 17px',
+          borderColor: 'transparent transparent darkgrey transparent',
+          boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.8)',
+        });
+      },
     };
   });
 
